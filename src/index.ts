de5814452cbe5fdeca87d0704bd61039063c4416
A Zakharov <pro@0x3a.xyz>
2024-06-12 19:27:53 +0300

    scale

--- src/index.ts
+++ src/index.ts
@@ -124,9 +124,13 @@ class ExampleScene extends Scene
 
 const game = new Game({
     type   : Phaser.AUTO,
-    width  : 800,
-    height : 600,
     scene  : ExampleScene,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     physics: {
         default: 'arcade',
         arcade : {
