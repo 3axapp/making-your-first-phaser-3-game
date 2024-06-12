6814ec37c8b3e0c6b899a49156316a021fccce35
A Zakharov <pro@0x3a.xyz>
2024-06-12 16:58:28 +0300

    part 5

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "phaser";
 import {Game, Scene} from "phaser";
 import StaticGroup = Phaser.Physics.Arcade.StaticGroup;
+import SpriteWithDynamicBody = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
 var platforms: StaticGroup;
+var player: SpriteWithDynamicBody;
 
 class ExampleScene extends Scene
 {
@@ -29,6 +31,31 @@ class ExampleScene extends Scene
         platforms.create(600, 400, 'ground');
         platforms.create(50, 250, 'ground');
         platforms.create(750, 220, 'ground');
+
+        player = this.physics.add.sprite(100, 450, 'dude');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
     }
 
     public update()
