27ff3bc3ce71b6e96c2f4361ac746ead12f3e3a8
A Zakharov <pro@0x3a.xyz>
2024-06-12 17:14:21 +0300

    part 8

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import {Game, Scene} from "phaser";
 import StaticGroup = Phaser.Physics.Arcade.StaticGroup;
 import SpriteWithDynamicBody = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+import Group = Phaser.GameObjects.Group;
+import SpriteWithStaticBody = Phaser.Types.Physics.Arcade.SpriteWithStaticBody;
 
 var platforms: StaticGroup;
 var player: SpriteWithDynamicBody;
 var cursors: CursorKeys;
+var stars: Group;
 
 class ExampleScene extends Scene
 {
@@ -40,53 +43,62 @@ class ExampleScene extends Scene
         player.setCollideWorldBounds(true);
 
         this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            key      : 'left',
+            frames   : this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
             frameRate: 10,
-            repeat: -1
+            repeat   : -1
         });
 
         this.anims.create({
-            key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
+            key      : 'turn',
+            frames   : [{key: 'dude', frame: 4}],
             frameRate: 20
         });
 
         this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            key      : 'right',
+            frames   : this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
             frameRate: 10,
-            repeat: -1
+            repeat   : -1
         });
 
         this.physics.add.collider(player, platforms);
 
         cursors = this.input.keyboard!.createCursorKeys();
+
+        stars = this.physics.add.group({
+            key   : 'star',
+            repeat: 11,
+            setXY : {x: 12, y: 0, stepX: 70}
+        });
+
+        stars.children.iterate(function (child: SpriteWithStaticBody)
+        {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        } as any);
+
+        this.physics.add.collider(stars, platforms);
+
+        this.physics.add.overlap(player, stars, collectStar as any, undefined, this);
     }
 
     public update()
     {
-        if (cursors.left.isDown)
-        {
+        if (cursors.left.isDown) {
             player.setVelocityX(-160);
 
             player.anims.play('left', true);
-        }
-        else if (cursors.right.isDown)
-        {
+        } else if (cursors.right.isDown) {
             player.setVelocityX(160);
 
             player.anims.play('right', true);
-        }
-        else
-        {
+        } else {
             player.setVelocityX(0);
 
             player.anims.play('turn');
         }
 
-        if (cursors.up.isDown && player.body.touching.down)
-        {
+        if (cursors.up.isDown && player.body.touching.down) {
             player.setVelocityY(-330);
         }
     }
@@ -105,3 +117,8 @@ const game = new Game({
         }
     },
 });
+
+function collectStar (player: SpriteWithDynamicBody, star: SpriteWithStaticBody)
+{
+    star.disableBody(true, true);
+}
