67427ef7c3effaba345e51b9cf8187434d4e206d
A Zakharov <pro@0x3a.xyz>
2024-06-12 16:39:41 +0300

    part 1

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import "phaser";
+import {Game, Scene} from "phaser";
+
+class ExampleScene extends Scene
+{
+    public preload()
+    {
+
+    }
+
+    public create()
+    {
+
+    }
+
+    public update()
+    {
+
+    }
+}
+
+const game = new Game({
+    type  : Phaser.AUTO,
+    width : 800,
+    height: 600,
+    scene : ExampleScene,
+});
